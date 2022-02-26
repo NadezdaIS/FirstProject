52bd7a07d9b83e49adb1ed03c8a8861e17e04c08
NadezdaIS <nadezhdaivanova29@gmail.com>
2022-02-26 18:50:46 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// First Java code
+System.out.println("Hello World");
+float number = 2;
+float value = 6;
+
+        System.out.println("Addition of number and value:" + (number + value));
+        System.out.println("Subtraction of number and value:" + (number - value));
+        System.out.println("Division of number and value:" + (number / value));
+        System.out.println("Multiplication of number and value:" + (number * value));
+    }
+}
