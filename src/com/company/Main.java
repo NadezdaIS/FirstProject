f018c0b1b720e1454dca57fa6988a8914c631c4e
NadezdaIS <nadezhdaivanova29@gmail.com>
2022-03-06 20:49:39 +0200

    Strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,142 @@
 package com.company;
 
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// First Java code
-System.out.println("Hello World");
-float number = 2;
-float value = 6;
 
-        System.out.println("Addition of number and value:" + (number + value));
-        System.out.println("Subtraction of number and value:" + (number - value));
-        System.out.println("Division of number and value:" + (number / value));
-        System.out.println("Multiplication of number and value:" + (number * value));
+        /*System.out.println("Task 1: " + (6<9 && 9>2));
+        System.out.println("Task 2: " + (5>9 || 8<3));
+        System.out.println("Task 3: " + (8==2 && 7<6));*/
+
+        /*System.out.println("Task 1: " + (6<9 && 9>2));
+        System.out.println("Task 2: " + (5>9 || 8<3));
+        System.out.println("Task 3: " + (8==2 && 7<6));
+
+        int number = 15;
+        if (number > 9 && number < 14) {
+            System.out.println(number+ " is between 8 and 14");
+        }
+        else {
+        } System.out.println(number+ " is not between 8 and 14");*/
+
+
+       /*int number = 11;
+            if (number == 11) {
+                System.out.println("Number is 11.");
+            }
+            else if (number > 9 && number < 14) {
+                System.out.println(number+ " is between 9 and 14.");
+        }
+            else {
+        System.out.println(number+ " is not between 9 and 14.");
+            }*/
+        //int count = 0;
+        /*do {
+            System.out.println(count);
+            count = count + 1;
+        } while (count< 10);*/
+
+        // while (count < 10);
+        // {
+        //    System.out.println(count);
+        //    count = count + 1;
+        // }
+
+/*for (int i = 8; i < 15; i++) {
+            if (i > 9) {
+                System.out.println(i + "is greater then 9.");
+            }
+        }*/
+// Task 1
+/*for (int x = 1; x<50;x++) {
+    if (x % 2 == 0) {
+        System.out.println(x + " is even");
+    } else {
+        System.out.println(x + " is odd");
     }
 }
+
+ */
+//Task 2
+ /*       int number = 1;
+
+        while (number<50){
+            if (number%4==0){
+                 System.out.println(number);
+
+         }
+            number++;
+        }*/
+        //Task 3
+        /*for (int x = 1; x<=50;x++) {
+            if (x % 3 == 0 && x%7==0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (x%3==0){
+                System.out.println("Fizz");
+            }
+            else if (x%7==0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(x);
+            }
+        }*/
+        /*boolean prime = true;
+        for (int x = 1; x < 80; x++) {
+            if (x == 2 || x  == 3 || x  == 5 || x  == 7)
+            {
+                System.out.println(x);
+            }
+            if (x == 1 || x % 2 == 0|| x % 3 == 0|| x % 4 == 0|| x % 5 == 0|| x % 7 == 0)
+            {
+                prime = false;
+            }
+         else {
+            System.out.println(x);
+        }
+        }*/
+
+        /*char operator = '+';
+        float firstnumber = 1;
+        float secondnumber = 2;
+            if (operator == '+') {
+            System.out.println("Answer is " + (firstnumber + secondnumber));
+            }
+            if  (operator == '-') {
+            System.out.println("Answer is " + (firstnumber - secondnumber));
+            }
+            if (operator == '*') {
+            System.out.println("Answer is " + (firstnumber * secondnumber));
+            }
+            if (operator == '/') {
+            System.out.println("Answer is " + (firstnumber / secondnumber));
+            }*/
+        /*String firstWord = "Physician";
+        String secondWord = "Aerodynamics";
+        int counterFirst = 0;
+        int counterSecond = 0;
+        for (int i = 0; i < firstWord.length(); i++) {
+            // System.out.println(firstWord.toLowerCase().charAt(i));
+            if ('a' == firstWord.charAt(i) || 'o' == firstWord.charAt(i) || 'e' == firstWord.charAt(i)
+                    || 'i' == firstWord.charAt(i) || 'u' == firstWord.charAt(i)) {
+                counterFirst++;
+            }
+        }
+        for (int j = 0; j < secondWord.length(); j++) {
+            if ('a' == secondWord.charAt(j) || 'o' == secondWord.charAt(j) || 'e' == secondWord.charAt(j)
+                    || 'i' == secondWord.charAt(j) || 'u' == secondWord.charAt(j)) {
+                counterSecond++;
+            }
+        }
+        System.out.println("The number of vowels in the first word is " + counterFirst);
+        System.out.println("The number of vowels in the second word is " + counterSecond);*/
+
+
+
+
+    }
+    }
+
